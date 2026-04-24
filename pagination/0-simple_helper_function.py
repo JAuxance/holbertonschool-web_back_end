41588b477fd169887cd7fa17893d9c7cef567338
JAuxance <auxance.jourdan@proton.me>
2026-04-24 19:39:30 +0200

    Change file permissions to executable for NoSQL and pagination scripts

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -12,6 +12,6 @@ def index_range(page, page_size):
         Returns:
             tuple: A tuple containing the start index and end index for the page.
             """
-            start_index = (page - 1) * page_size
-            end_index = start_index + page_size
-            return (start_index, end_index)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
