208829efbb262a86d6f9fc14be2562081eb89657
JAuxace <auxance.jourdan@proton.me>
2026-05-13 19:11:01 +0200

    Add 7-http_express.js complex HTTP server with /students endpoint using Express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const lines = ['This is the list of our students'];
+  const originalLog = console.log;
+  console.log = (msg) => { lines.push(msg); };
+
+  countStudents(database)
+    .then(() => {
+      console.log = originalLog;
+      res.send(lines.join('\n'));
+    })
+    .catch((err) => {
+      console.log = originalLog;
+      res.send(`${lines.join('\n')}\n${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
